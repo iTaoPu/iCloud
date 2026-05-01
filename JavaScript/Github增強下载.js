5637b9314e9e56e4b4c075b8474ef586fdef1cbb
i叚娤倖鍢 <89057443+iTaoPu@users.noreply.github.com>
2026-05-02 00:13:51 +0800

    Update Github增強下载.js

--- JavaScript/Github增強下载.js
+++ JavaScript/Github增強下载.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github 增強 - 高速下載
 // @namespace    https://i叚娤.倖鍢.net.cn
-// @version      4.5.2
+// @version      5.5.5
 // @author       言氏稗客
 // @description  人的白嫖，就如同高山滚石一般，一旦开始，就再也停不下了 —— 「鲁迅」—— 精确 0.5px 对齐
 // @match        *://github.com/*
